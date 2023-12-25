d802bb2df2c29f948f321328a61c0496463cd8ef
maleterian <uladzimir.bartkevich@gmail.com>
2023-12-25 22:24:07 +0100

    Airflow task clean up

--- airflow/dags/docker_spark_dag_with_task_groups.py
+++ airflow/dags/docker_spark_dag_with_task_groups.py
@@ -13,28 +13,3 @@ with DAG('spark_submit_all_tasks', default_args=default_args, schedule_interval=
         return get_spark_submit_operator(group_id, task_id, dag)
 
     # Define task groups
-    with TaskGroup('group1') as processing_tasks_group1:
-        group1_task_1 = get_task_operator(1, 1)
-        group1_task_2 = get_task_operator(1, 2)
-
-        group1_task_1 >> group1_task_2
-
-    with TaskGroup('group2') as processing_tasks_group2:
-        group2_task_1 = get_task_operator(2, 1)
-        group2_task_2 = get_task_operator(2, 2)
-        group2_task_3 = get_task_operator(2, 3)
-        group2_task_4 = get_task_operator(2, 4)
-        group2_task_5 = get_task_operator(2, 5)
-
-        [group2_task_1, group2_task_2, group2_task_3] >> group2_task_4 >> group2_task_5
-
-    with TaskGroup('group3') as processing_tasks_group3:
-        group3_task_1 = get_task_operator(3, 1)
-        group3_task_2 = get_task_operator(3, 2)
-        group3_task_3 = get_task_operator(3, 3)
-        group3_task_4 = get_task_operator(3, 4)
-        group3_task_5 = get_task_operator(3, 5)
-
-    # Define dependencies between task groups
-    processing_tasks_group1.set_downstream(processing_tasks_group2)
-    processing_tasks_group2.set_downstream(processing_tasks_group3)
